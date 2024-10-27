7a101cf2a228c1d25d7fd1c79092ebf9287416fe
zakariaevip <84985980+zakariaeVIP@users.noreply.github.com>
2024-10-27 22:11:00 +0100

    Add files via upload

---
+++ CCNA3/course/module6/6.4.1.4/media/media_6.4.1.4_page.js
@@ -0,0 +1,12 @@
+
+function registerSWF(){
+	swfobject.registerObject("flashobject", "9.0.0", "../../../common/libs/expressInstall.swf");
+}
+
+
+loadScript("../../../common/scripts/swfobject.js", registerSWF);
+
+slideViewDidLoad = function(_container,_id)
+{
+isResize=false;
+}
