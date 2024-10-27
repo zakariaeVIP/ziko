71bff5c6449054eddae994d759abb7f2cbdfbcc1
zakariaevip <84985980+zakariaeVIP@users.noreply.github.com>
2024-10-27 08:25:39 +0100

    Delete CCNA3/course/module3 directory

--- CCNA3/course/module3/3.1.2.3/media/media_3.1.2.3_page.js
+++
@@ -1,7 +0,0 @@
-
-function registerSWF(){
-	swfobject.registerObject("flashobject", "9.0.0", "../../../common/libs/expressInstall.swf");
-}
-
-
-loadScript("../../../common/scripts/swfobject.js", registerSWF);
--- CCNA3/course/module3/3.1.2.4/media/override.js
+++
@@ -1,2 +0,0 @@
-override.pageType = "text-on-right"; // "text-on-left", "text-on-right", "full-media"
-override.pageSize="470*400";
