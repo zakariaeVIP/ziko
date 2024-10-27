74abe4ab6f0f650f7168589d4166449de9fea44a
zakariaevip <84985980+zakariaeVIP@users.noreply.github.com>
2024-10-27 08:31:41 +0100

    Add files via upload

---
+++ CCNA3/course/module3/3.1.1.4/media/media_3.1.1.4_page.js
@@ -0,0 +1,23 @@
+
+function registerSWF(){
+	swfobject.registerObject("flashobject", "9.0.0", "../../../common/libs/expressInstall.swf");
+}
+loadScript("../../../common/scripts/swfobject.js", registerSWF);
+
+(Animation = function(control,id){
+		
+	
+	var manifest = [
+		{src:"images/envelope_closed_static.png", id:"envelope_closed_static"},
+		{src:"images/media_4114.png", id:"media_4114"}
+	];
+
+
+	return manifest;			
+});
+
+
+(slideViewDidLoad = function(slideContainer,id){
+var c=document.getElementById("canvas");
+c.height=490;
+});
